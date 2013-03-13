b4203050c9d610c738e03ef860c079b3242fcf74
Charles Feduke <charles.feduke@gmail.com>
2013-03-13 16:56:22 -0400

    against better judgement and author recommendations, updated all the dependencies to their latest versions as of 2013-03-13 because I like programming on the edge and frustration is my friend

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject programming-clojure "1.3.0"
   :description "Examples from Programming Clojure"
   :aot [examples.tasklist]
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/java.jdbc "0.0.6"]
-                 [org.clojure/test.generative "0.1.2"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.clojure/test.generative "0.4.0"]
                  [ring "0.3.11" :exclusions [org.clojure/clojure
                                              org.clojure/clojure-contrib]]
-                 [compojure "0.6.5"]
-                 [hiccup "0.3.6"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.2"]
                  [jline "0.9.94"]
                  [hsqldb "1.8.0.10"]])
